32c0a7d490c4a861dda6a9d528350af7b23354ff
James Yi <james.yi@sterlingts.com>
2018-07-10 15:01:08 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+    desc: 'A New Site'
+  },
+  pathPrefix: "/gatsby",
+  plugins: [
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-styled-components',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'src',
+      path: `${__dirname}/src/posts/`
+    }
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'img',
+      path: `${__dirname}/src/images`
+    }
+  },
+  'gatsby-transformer-remark',
+  'gatsby-transformer-sharp',
+  'gatsby-plugin-sharp'
+  ]
+};
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae porro pariatur perferendis fugiat doloremque inventore atque error possimus, cumque animi. Amet tenetur similique quos, consectetur dolores minima excepturi fugiat iusto.</p>
+      </div>
+    )
+  }
+}
+
+export default About;
